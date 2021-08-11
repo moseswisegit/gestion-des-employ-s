a89aa88fe1b0a51961fb9449650b642b34cf8bfe
Moses Wise <moseswisegit@gmail.com>
2021-08-11 18:12:20 +0100

    finish

---
+++ app/Models/Employee.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\City;
+use App\Models\State;
+use App\Models\Country;
+use App\Models\Department;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'middle_name',
+         'last_name',
+         'address',
+         'country_id',
+         'state_id',
+         'city_id',
+         'department_id',
+         'zip_code',
+         'birthdate',
+         'date_hired'
+        ];
+   protected $cast = [
+       'birthdate'  => 'datetime:Y-m-d',
+
+       'date_hired' => 'datetime:Y-m-d',
+   ];
+
+   public function country(){
+       return $this->belongsTo(Country::class);
+   }
+   public function state(){
+       return $this->belongsTo(State::class);
+   }
+
+   public function city(){
+       return $this->belongsTo(City::class);
+   }
+
+   public function department(){
+       return $this->belongsTo(Department::class);
+   }
+}  
